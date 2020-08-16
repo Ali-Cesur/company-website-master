469c73fa2325e507cbaeeb78bb8d61c75ee308b1
rog <alicesur.ytu@gmail.com>
2020-08-16 14:58:13 +0300

    MaterialUI added

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container'
+import { Button } from '@material-ui/core';
 
 export default () => {
     return (
         <div>
             <h2>About Page</h2>
             <p>Lorem ipsum goes here</p>
+            <Button color="primary">Hello World</Button>;
         </div>
     )
 }
--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default () => {
     return (
         <div>
             <h2>Contact Page</h2>
             <p>Lorem ipsum goes here</p>
+            <Button color="primary">Hello World</Button>;
+
         </div>
     )
 }
